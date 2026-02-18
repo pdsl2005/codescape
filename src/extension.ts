33fc7e783140541ef0fe3509b2b7d249fbaed508
Logan Tom <tom.lo@northeastern.edu>
2026-02-17 19:52:35 -0500

    removed unused import

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { FileParseStore } from './state';
-import { parseAndStore, parseAndStoreArray } from './parser';
+import { parseAndStore} from './parser';
 import { minimatch } from 'minimatch';
 
 // This method is called when your extension is activated
